7b1e72c77159d3cd83e11a352ca6a096ea4d8d58
ellenliu <ellen.liu19@gmail.com>
2020-02-06 23:06:14 -0800

    Initial Commit

---
+++ Color_Match_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Color_Match_App
+//
+//  Created by Ellen Liu on 2020-02-06.
+//  Copyright © 2020 Ellen Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Color_Match_AppTests/Color_Match_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Color_Match_AppTests.swift
+//  Color_Match_AppTests
+//
+//  Created by Ellen Liu on 2020-02-06.
+//  Copyright © 2020 Ellen Liu. All rights reserved.
+//
+
+import XCTest
+@testable import Color_Match_App
+
+class Color_Match_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

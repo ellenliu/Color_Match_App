8f7b2d08fc8df41d99d2df7f3aff3c0bd9237166
ellenliu <ellen.liu19@gmail.com>
2020-02-07 23:39:59 -0800

    can change view background color

--- Color_Match_App/GameViewController.swift
+++ Color_Match_App/GameViewController.swift
@@ -11,11 +11,15 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet weak var userCanvas: UIView!
+    @IBOutlet weak var goalCanvas: UIView!
+    
     @IBAction func backToMenuButton(_ sender: Any) {
         self.performSegue(withIdentifier: "backToMenuSegue", sender: self)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        userCanvas.backgroundColor = UIColor.red
         // Do any additional setup after loading the view.
     }
 

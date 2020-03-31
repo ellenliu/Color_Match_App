28641f55258eefa609ccce47b2a46c6bb4ce584f
ellenliu <ellen.liu19@gmail.com>
2020-03-30 17:31:38 -0700

    added text

--- Color_Match_App/WikiView.swift
+++ Color_Match_App/WikiView.swift
@@ -12,11 +12,22 @@ import SwiftUI
 struct WikiView: View {
     
     var body: some View {
-        VStack(spacing: 10) {
-            Text("Hello ") + Text("World!").bold()
+        VStack(alignment: .leading, spacing: 10) {
+            Text("What is Color?").font(.system(size: 30)).bold()
+            
+            Text("Light is differentially reflected at each wavelength, which gives " +
+                "objects their natural color. Color is not a property of light, but rather " +
+                "an artifact of human perception. What we call 'color' is how we perceive light's wavelength.")
+            
+            Text("How to Play").font(.system(size: 30)).bold()
+            
+            Text("Most colors can be represented as a positive sum of the three primary colors. " +
+                "This is called additive mixing. You will be given a split screen with the goal color on " +
+                "one panel, and the objective of the game is to recreate the color on the other panel " +
+                "with the correct mixutre of the three primary colors.")
+            
         }
-            .textFieldStyle(RoundedBorderTextFieldStyle())
-            .padding([.leading, .trailing], 40)
+            .padding([.leading, .trailing], 30)
     }
 }
 

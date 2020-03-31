e39701ae7663e3ccfb5eac12cfceaedb55c15aae
ellenliu <ellen.liu19@gmail.com>
2020-03-30 17:56:24 -0700

    added spectrum pic

--- Color_Match_App/WikiView.swift
+++ Color_Match_App/WikiView.swift
@@ -19,13 +19,14 @@ struct WikiView: View {
                 "objects their natural color. Color is not a property of light, but rather " +
                 "an artifact of human perception. What we call 'color' is how we perceive light's wavelength.")
             
+            Image("color-spectrum").resizable().scaledToFit()
+            
             Text("How to Play").font(.system(size: 30)).bold()
             
             Text("Most colors can be represented as a positive sum of the three primary colors. " +
                 "This is called additive mixing. You will be given a split screen with the goal color on " +
                 "one panel, and the objective of the game is to recreate the color on the other panel " +
                 "with the correct mixutre of the three primary colors.")
-            
         }
             .padding([.leading, .trailing], 30)
     }
